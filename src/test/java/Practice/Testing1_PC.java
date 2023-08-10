113c3602403f40b3a7e1900e7215657e21437216
BeckPC <beckkhorezmiy@gmail.com>
2023-08-09 21:39:55 -0500

    added method2 and 2 variables

--- src/test/java/Practice/Testing1_PC.java
+++ src/test/java/Practice/Testing1_PC.java
@@ -1,7 +1,15 @@
 package Practice;
 
 public class Testing1_PC {
+
+    private String name;
+    private String address;
     public static void main(String[] args) {
         System.out.println(Testing1_PC.class.getName());
     }
+
+    public static void mothod2(){
+        System.out.println("inside method2");
+    }
+
 }
